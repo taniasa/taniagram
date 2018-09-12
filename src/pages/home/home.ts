e5ee5ba969407c171f62b338ddb115fd1e988b24
Tânia Salgado de Abreu <taniasgdo@hotmail.com>
2018-09-12 01:17:22 -0300

    Até aula 8 prototipação de fotos.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoginPage } from './../login/login';
+import { PhotosPage } from './../photos/photos';
 
+import { Component } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
+// import { PhotosPage } from '../photos/photos';
+// import { TakePicturePage } from '../take-picture/take-picture';
+// import { ProfilePage } from '../profile/profile';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public photosTab: any;
+  public profileTab: any;
+  public user: string;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(
+    private navCtrl: NavController,
+    private modalCtrl: ModalController) {
+    this.photosTab = PhotosPage;
+    this.profileTab = LoginPage;
+    // this.photosTab = PhotosPage;
+    // this.profileTab = ProfilePage;
   }
 
+  showSendPhoto() {
+    // let modal = this.modalCtrl.create(TakePicturePage);
+    // modal.present();
+  }
 }
